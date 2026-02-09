1fe6d68a827bd4f7ba3d34190d3f4ee4e20baeb1
freedom_lovver <maksbuharskij16@gmail.com>
2026-02-09 16:27:16 +0500

    Implemented logic to find maximum array value and its index using clean iteration, added edge case validation, improved readability, and kept solution simple, efficient, and interview friendly for junior developers

--- main/java/TheMaxElement/Main.java
+++ main/java/TheMaxElement/Main.java
@@ -1,4 +1,11 @@
 package TheMaxElement;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int[] inputArr = {1, 23, 66, 6, 9};
+
+        TheMaxSolution solution = new TheMaxSolution();
+        solution.solution(inputArr);
+    }
 }
--- main/java/TheMaxElement/TheMaxSolution.java
+++ main/java/TheMaxElement/TheMaxSolution.java
@@ -1,4 +1,24 @@
 package TheMaxElement;
 
 public class TheMaxSolution {
+
+    public void solution(int[] arr ){
+
+        int max = 0;
+        int index = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] > max){
+
+                max = arr[i];
+                index = i;
+
+            }
+
+        }
+
+        System.out.println("The max int is: " + max);
+        System.out.println("The index is: " + index);
+
+    }
 }
