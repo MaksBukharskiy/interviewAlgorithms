f6644bd7ba43691fbe79ad48a776668dc2753f95
freedom_lovver <maksbuharskij16@gmail.com>
2026-02-10 17:57:02 +0500

    Add algorithm to find min

--- main/java/TheMaxElement/Main.java
+++ main/java/TheMaxElement/Main.java
@@ -3,7 +3,7 @@ package TheMaxElement;
 public class Main {
     public static void main(String[] args) {
 
-        int[] inputArr = {1, 23, 66, 6, 9};
+        int[] inputArr = {1, 23, 66, 6, 0};
 
         TheMaxSolution solution = new TheMaxSolution();
         solution.solution(inputArr);
--- main/java/TheMaxElement/TheMaxSolution.java
+++ main/java/TheMaxElement/TheMaxSolution.java
@@ -1,24 +1,40 @@
 package TheMaxElement;
 
+import java.util.Arrays;
+
 public class TheMaxSolution {
 
     public void solution(int[] arr ){
 
-        int max = 0;
-        int index = 0;
+        if (arr == null || arr.length == 0){
+            throw  new IllegalArgumentException("The arrayc is empty");
+        }
+
+        int index1 = 0;
+        int index2 = 0;
+
+        int max = arr[0];
+        int min = arr[0];
 
         for(int i = 0; i < arr.length; i++){
+
+            if(arr[i] < min ){
+                min = arr[i];
+                index1 = i;
+
+            }
             if (arr[i] > max){
-
                 max = arr[i];
-                index = i;
-
+                index2 = i;
             }
 
         }
 
         System.out.println("The max int is: " + max);
-        System.out.println("The index is: " + index);
+        System.out.println("The index is: " + index2);
+
+        System.out.println("\nThe min int is: " + min);
+        System.out.println("The index is: " + index1);
 
     }
 }
